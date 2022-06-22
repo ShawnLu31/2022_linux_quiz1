978b7fed9487ab694351962d7b68539a9c7071ba
ShawnLu31 <john2001131@gmail.com>
2022-06-22 22:55:18 +0800

    first commit

---
+++ test2/iteration.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "list.h"
+
+struct ListNode {
+    int val;
+    struct list_head *prev, *next;
+};
+
+struct list_head *deleteDuplicates(struct list_head *head);
+
+int main(int argc, char *argv[]){
+    
+    return 0;
+}
+
+struct list_head *deleteDuplicates(struct list_head *head)
+{
+
+}
---
+++ test2/recursive.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "list.h"
+
+struct ListNode {
+    int val;
+    struct list_head *prev, *next;
+};
+
+int main(int argc, char *argv[]){
+    
+    return 0;
+}
